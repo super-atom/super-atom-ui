315c6663e4619ed298582da76b02d05750cfe3b3
JUSEOK LEE <super0atom@gmail.com>
2021-11-24 17:58:01 +0900

    페이지 생성 npm script 작성

---
+++ scripts/create-page.js
@@ -0,0 +1,16 @@
+const PATHS = require('../config/paths');
+const process = require('process');
+const path = require('path');
+const fs = require('fs');
+const folderName = process.argv[2] || 'Project';
+
+try {
+  fs.mkdirSync(path.join(PATHS.source, 'pages', folderName));
+  const exts = ['json', 'pug', 'scss', 'js'];
+  for (const ext of exts) {
+    fs.writeFileSync(`sources/pages/${folderName}/${folderName}.${ext}`, '');
+  }
+} catch (err) {
+  console.log(`Error!`);
+  console.log(err);
+}
