79d6729dfce3999f896898a38246ed93463141c6
JUSEOK LEE <super0atom@gmail.com>
2021-12-08 17:03:01 +0900

    불필요한 믹스인 제거 및 정리

--- src/scripts/ts/common.ts
+++ src/scripts/ts/common.ts
@@ -1,3 +1,9 @@
 import '../../styles/common.scss';
 import './motion/gsap';
 console.log('COMMON');
+
+const $nav = $('.nav');
+const $nav_toggle_btn = $('.js-nav__toggle-btn');
+$nav_toggle_btn.on('click', () => {
+  $nav.toggleClass('on');
+});
