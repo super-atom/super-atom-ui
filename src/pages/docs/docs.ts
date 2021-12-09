dade033cb1a427724b7ffcdbb4ab653e2214fe29
JUSEOK LEE <super0atom@gmail.com>
2021-12-09 17:52:53 +0900

    오피셜 페이지 스타일 작업

--- src/pages/docs/docs.ts
+++ src/pages/docs/docs.ts
@@ -1,1 +1,14 @@
 import './docs.scss';
+
+const $js_btn: JQuery<HTMLElement> = $('.js_button');
+const $js_aside_nav_btn = $('.js_aside_nav_btn');
+const $js_nav__link_depth_1_wrap = $('.js_nav__link_depth_1_wrap');
+$js_btn.on('click', (e) => {
+  if (e.target.parentElement.classList[0] === 'nav__link-depth-1') {
+    e.target.parentElement.classList.toggle('on');
+  }
+});
+
+$js_aside_nav_btn.on('click', () => {
+  $js_nav__link_depth_1_wrap.toggleClass('on');
+});
