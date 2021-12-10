dc80ef1d23f5b39de009f25cac9fc1e6a1901831
JUSEOK LEE <super0atom@gmail.com>
2021-12-10 17:35:06 +0900

    오피셜 페이지 스타일 작업 진행중

--- src/pages/docs/docs.ts
+++ src/pages/docs/docs.ts
@@ -1,8 +1,18 @@
 import './docs.scss';
+import MobileDetect from 'mobile-detect';
+
+const md = new MobileDetect(window.navigator.userAgent);
+const isMobile = md.userAgent() !== null;
 
 const $js_btn: JQuery<HTMLElement> = $('.js_button');
 const $js_aside_nav_btn = $('.js_aside_nav_btn');
 const $js_nav__link_depth_1_wrap = $('.js_nav__link_depth_1_wrap');
+const $js_nav__link_depth_1 = $('.js_nav__link_depth_1');
+
+if (!isMobile) {
+  $js_nav__link_depth_1.addClass('on');
+}
+
 $js_btn.on('click', (e) => {
   if (e.target.parentElement.classList[0] === 'nav__link-depth-1') {
     e.target.parentElement.classList.toggle('on');
@@ -11,4 +21,7 @@ $js_btn.on('click', (e) => {
 
 $js_aside_nav_btn.on('click', () => {
   $js_nav__link_depth_1_wrap.toggleClass('on');
+  if ($js_nav__link_depth_1.hasClass('on')) {
+    $js_nav__link_depth_1.toggleClass('on');
+  }
 });
